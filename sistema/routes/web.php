17d44c9ac135d68999624f9d8e7ca24c7d81fa12
Ricardo Diaz <devinnovatechcorp@gmail.com>
2025-09-14 18:06:28 -0400

    Se agregaron componentes, directivas y rutas requeridas

--- sistema/routes/web.php
+++ sistema/routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('template');
 });
+Route::view('/panel','panel.index')->name('panel');
 Route::get('/login', function () {
     return view('auth.login');
 });
