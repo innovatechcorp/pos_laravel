7bad378689d392a933551ad30daea34126a3abc6
Ricardo Diaz <devinnovatechcorp@gmail.com>
2025-09-14 11:58:36 -0400

    Creacion de Relaciones(Modelos)

--- sistema/app/Models/Compra.php
+++ sistema/app/Models/Compra.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Compra extends Model
 {
     use HasFactory;
+
+    public function proveedore(){
+        return $this->belongsTo(Proveedore::class);
+    }
+
+    public function comprobante(){
+        return $this->belongsTo(Comprobante::class);
+    }
+
+    public function productos(){
+        return $this->belongsToMany(Producto::class)->withTimestamps()->withPivot('cantidad','precio_compra','precio_venta');
+    }
 }
--- sistema/app/Models/Producto.php
+++ sistema/app/Models/Producto.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Producto extends Model
 {
     use HasFactory;
+
+    public function compras(){
+        return $this->belongsToMany(Compra::class)->withTimestamps()->withPivot('cantidad','precio_compra','precio_venta');
+    }
+    public function ventas(){
+        return $this->belongsToMany(Venta::class)->withTimestamps()->withPivot('cantidad','precio_venta','descuento');
+    }
+    public function categorias(){
+        return $this->belongsToMany(Categoria::class)->withTimestamps();
+    }
+    public function marca(){
+        return $this->belongsToMany(Marca::class);
+    }
+    public function presentacione(){
+        return $this->belongsToMany(Presentacione::class);
+    }
 }
