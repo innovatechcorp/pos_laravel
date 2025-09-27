039a475282cd04818429213386172a2d87f7d14e
Ricardo Diaz <devinnovatechcorp@gmail.com>
2025-09-26 20:56:35 -0400

    Listado proveedores en una tabla usando el método INDEX.

--- sistema/resources/views/proveedore/index.blade.php
+++ sistema/resources/views/proveedore/index.blade.php
@@ -44,11 +44,69 @@
                             </div>
                             <div class="card-body">
                                 <table id="datatablesSimple" class="table table-striped">
-                                    <div class="modal-dialog">
+                                    <thead>
+                                    <tr>
+                                        <th>Nombre</th>
+                                        <th>Direccion</th>
+                                        <th>Tipo_documento</th>
+                                        <th>Nro Documento</th>
+                                        <th>Estado</th>
+                                        <th>Acciones</th>
+                                    </tr>
+                                   </thead>
+                                   <tbody>
+                                    @foreach ($proveedores as $item)
+                                        <tr>
+                                            <td>{{$item->persona->razon_social}}</td>
+                                            <td>{{$item->persona->direccion}}</td>
+                                            <td>{{$item->persona->documento->tipo_documento}}</td>
+                                            <td>{{$item->persona->numero_documento}}</td>
+                                            <td>{{$item->persona->tipo_persona}}</td>
+                                            <td>
+                                        @if ($item->persona->estado==1)
+                                            <span class="badge bg-success d-inline text-white">Activo</span>
+                                        @else
+                                             <span class="badge bg-danger d-inline text-white">Elimado</span>
+                                        @endif
+                    </td>
+                    <td><div class="btn-group" role="group" aria-label="Basic mixed styles example">
+                            <form action="{{route('proveedores.edit',['proveedore'=>$item])}}" method="GET">
+                                <button type="submit" class="btn btn-warning">Editar</button>
+                            </form>
+                            @if ($item->persona->estado==1)
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#confirmModal-{{$item->id}}">Eliminar</button>
+                            @else
+                                <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#confirmModal-{{$item->id}}">Restaurar</button>
+                            @endif
+                        
+                        
+                        </div>
+                    </td>
+                                        </tr>
+                                                                                           <!-- Modal -->
+<div class="modal fade" id="confirmModal-{{$item->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        {{$item->persona->estado==1 ? '¿Seguro  que quieres eliminar este cliente?' : 'Seguro  que quieres restaurar este cliente?' }}
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        <form action="{{route('clientes.destroy',['cliente'=>$item->persona->id])}}" method="POST">
+            @method('DELETE')
+            @csrf
+        <button type="submit" class="btn btn-danger">Confirmar</button>
+        </form>
+      </div>
     </div>
   </div>
 </div>
-                                      
+                                    @endforeach
+                                   </tbody>
                                     
                                 </table>
                             </div>
